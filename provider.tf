d3f82ab2fe447a67f21d267a0468c5c7f4a10b24
Rich Bos <cirronix@icloud.com>
2020-03-20 19:52:44 +0000

    Reformatted for v12

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 ## AWS details
 
 provider "aws" {
-  version = "~> 2.0"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region}"
+  version    = "~> 2.0"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
