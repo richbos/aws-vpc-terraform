48273b8f1059348ad37641ae785f9615f5a17c78
Rich Bos <cirronix@icloud.com>
2020-03-20 20:40:43 +0000

    v12 revised interpolation

--- vpc.tf
+++ vpc.tf
@@ -61,7 +61,7 @@ resource "aws_route_table" "pub" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.igw.id}"
+    gateway_id = "aws_internet_gateway.igw.id"
   }
 
   tags = {
